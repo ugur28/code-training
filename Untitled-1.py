f46b085c473380c3fc0dd12f3832e3ece5002024
ugur28 <ugur.hazar28@gmail.com>
2023-08-26 01:23:08 +0300

    denk o

--- Untitled-1.py
+++
@@ -1,7 +0,0 @@
-while True:
-    s1=int(input("alt alta yazdırmak istediğin sayı"))
-    if s1<=0:
-        print("sıfır veya sıfırdan küçük sayı girmeyiniz")
-    else:
-        for x in range (1,s1):
-            print(x)
--- deneme2.py
+++
@@ -1,1 +0,0 @@
-print("buralar çok karmaşık ")
