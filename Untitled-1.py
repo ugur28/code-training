ae45e7a93c3adc5495ef2d69ae9b144b34724fad
ugur28 <ugur.hazar28@gmail.com>
2023-08-26 01:00:51 +0300

    two

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,8 +1,1 @@
-while True:
-    s1=int(input("alt alta yazdırmak istediğin sayı"))
-    if s1<=0:
-        print("sıfır veya sıfırdan küçük sayı girmeyiniz")
-    else:
-        for x in range (1,s1):
-            print(x)
-            print(x)
+print("Saat 00.58 eskişehir'de skylab topluğunun düzenlediği etkinlikteyim.")
